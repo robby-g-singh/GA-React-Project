03dba44672be9a6844d75aa05684759de376c706
Robby Singh <robbysingh@Robbys-iMac.attlocal.net>
2022-05-16 15:47:40 -0500

    commit working react router routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import About from './Components/About';
 import Authors from './Components/Authors';
 import Home from './Components/Home'
 import Quotes from './Components/Quotes';
+import {Route, Routes, Link} from 'react-router-dom'
 
   fetch('https://type.fit/api/quotes')
     .then(response => response.json())
@@ -12,10 +13,25 @@ import Quotes from './Components/Quotes';
 function App() {
   return (
     <div className="App">
-      <Home />
-      <About />
-      <Quotes />
-      <Authors />
+      <nav className="nav-bar">
+        <ul className="nav-list">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/quotes">Quotes</Link>
+          <Link to="/authors">Authors</Link>
+        </ul>
+      </nav>
+
+      <main>
+        <Routes>
+          <Route path="" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/quotes" element={<Quotes />} />
+          <Route path="/authors" element={<Authors />} />
+
+        </Routes>
+      </main>
+      
       
       
     </div>
