8a15a11816c39f057267617b86dee7399fa0b9f7
Robby Singh <robbysingh@Robbys-iMac.attlocal.net>
2022-05-18 17:40:39 -0500

    commit id changes

--- src/Components/Authors.js
+++ src/Components/Authors.js
@@ -4,8 +4,8 @@ import '../stylesheets/Authors.css'
 const Authors = () => {
     return (
         <div className="authors-page">
-            <h1>Authors Page</h1>
-            <h2>View the featured authors in this API!</h2>
+            <h1 id="authors-heading">Authors Page</h1>
+            <h2 id="authors-subheading">View the featured authors in this API!</h2>
             <ul className="authors-list">
                 <AuthorsData />
             </ul>
